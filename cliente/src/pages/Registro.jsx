9ab63d07808f0e548950c22db50f528edcf7fe50
Duda-ifms <et49181@gmail.com>
2025-01-25 02:30:32 -0300

    25/01

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,5 +1,19 @@
-import { useState } from "react";
+import {useEffect, useState } from "react";
+import jsPDF from 'jsPDF';
+import 'jspdf-autotable';
 
+export default function Home(){
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+      const buscarUsuario = async () => {
+        try{
+          const resposta = await fetch("http://localhost:3000/usuarios");
+          const dados = await resposta.json();
+          setUsuarios(dados);
+           }catch{ 
+ }}})
+}
 export default function Registrar() {
   const[nome,setNome] = useState('');
   const[preco,setPreco] = useState('');
